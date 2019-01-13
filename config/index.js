3412c496ca22f1960f1d7c8687b0ec1d151d8332
Chandra Mishra <Prasadchandra18@gmail.com>
2019-01-13 15:21:26 +0530

    Delete index.js

--- config/index.js
+++
@@ -1,7 +0,0 @@
-var configValues = require('./config.json');
-
-module.exports = {
-    getDbConnectionString : function(){
-      return 'mongodb://'+configValues.user+':'+configValues.pwd+'@ds255794.mlab.com:55794/employee1';
-}
-}
